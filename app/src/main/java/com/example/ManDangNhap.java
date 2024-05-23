3f589516922762d4a9fb6ca908e9209550fba676
HUNGUTC <hungvhutc281@gmail.com>
2024-05-23 12:50:46 +0700

    BaiTapLon

---
+++ app/src/main/java/com/example/ManDangNhap.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_doctruyen.R;
+
+public class ManDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_dang_nhap);
+    }
+}
